cc52c53bb76312ec1b399cb43fae96530abb5c42
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-02-27 23:49:42 +0530

    Add files via upload

--- Day4_CanPlaceFlowers.java
+++ Day4_CanPlaceFlowers.java
@@ -7,33 +7,21 @@ public class Day4_CanPlaceFlowers {
 int[]flowerbed= {0,1,0};
 System.out.println(canPlaceFlowers(flowerbed, 1));
 	}
-	public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count=0;
-        for(int i=0;i<flowerbed.length;i++)
-        {
-            if(i==0&&flowerbed[i]==0&& flowerbed[i+1]==0)
-            {
-               count++;
-               System.out.println("1");
-            }
-            
-            else
-            if(i==flowerbed.length-1&& flowerbed[i]==0&&flowerbed[i-1]==0)
-            {
-                count++;
-                System.out.println("3"); 	
-            }
-            else
-                if(flowerbed[i]==0&&flowerbed[i+1]==0&&flowerbed[i-1]==0)
-                {
-                    count++;
-                    ++i;
-                    System.out.println("2");
-                    System.out.println(count+" "+i);
-                }
-
-        }
-        return count==n;
-        
-    }
+	 public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+	        int l = flowerbed.length;
+	        if(n == 0) return true;
+	        int prev = 0;
+	        for (int i = 0; i < l-1; i++) {
+	            if((prev | flowerbed[i] | flowerbed[i+1]) == 0) { 
+	                n--;
+	                flowerbed[i] = 1;
+	                if(n == 0) return true;
+	            }
+	            prev = flowerbed[i];
+	        }
+	        if((prev | flowerbed[l-1]) == 0) {
+	            n--;
+	        }
+	        return n <= 0;
+	    }
 }
