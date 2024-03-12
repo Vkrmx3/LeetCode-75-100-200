a8b1995b21cd0dae145c059c3b1cd1b2540ad327
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-03-13 00:43:26 +0530

    Add files via upload

--- Day18_RemoveDuplicatesFromSortedArray.java
+++ Day18_RemoveDuplicatesFromSortedArray.java
@@ -1,10 +1,25 @@
 package LeetCode75;
 
+import java.util.Arrays;
+
 public class Day18_RemoveDuplicatesFromSortedArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		System.out.println(removeDuplicates(nums));
 	}
 
+	public static int removeDuplicates(int[] nums) {
+        if (nums.length == 1)
+            return 1;
+        int left = 0, right = 1;
+        while (right < nums.length) {
+            if (nums[left] != nums[right])
+                nums[++left] = nums[right++];
+
+            else
+                right++;
+        }
+        return ++left;
+    }
 }
