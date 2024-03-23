fd696af03ab4e88ee1074b216dfa63cf287ab648
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-03-23 23:43:46 +0530

    Add files via upload

---
+++ Day29_MinimumMovesToConvertString.java
@@ -0,0 +1,26 @@
+package LeetCode75;
+
+public class Day29_MinimumMovesToConvertString {
+
+	public static void main(String[] args) {
+		System.out.println(minimumMoves("XX000XXXX"));
+	}
+	public static int minimumMoves(String s) {
+        int count=0;
+        if(s.contains("X"))
+        {
+        	for(int i=0;i<s.length();i++)
+        	{
+        		if(s.charAt(i)=='X')
+        		{
+        			count++;
+        			i+=2;
+        		}
+        	}
+        	return count;
+        }
+        else
+        	return 0;
+        
+    }
+}
