cdf07c40fcc936b83787d671f06548fe50781b82
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-04-05 23:48:03 +0530

    Add files via upload

---
+++ Day42_MakeTheStringGreat.java
@@ -0,0 +1,25 @@
+package LeetCode75;
+
+import java.util.Scanner;
+
+public class Day42_MakeTheStringGreat {
+
+	public static void main(String[] args) {
+		System.out.print(makeGood("abBAcC"));		
+	}
+	public static String makeGood(String s) {
+		
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0;i<sb.length()-1;i++)
+        {
+        	
+        	if(sb.charAt(i)-sb.charAt(i+1)==32 || sb.charAt(i)-sb.charAt(i+1)==-32)
+        	{
+//        	sb.replace(i, i+2,"");
+        	sb.delete(i, i+2);
+        	i=-1;
+        	}
+        }
+		return sb.toString();
+    }
+}
