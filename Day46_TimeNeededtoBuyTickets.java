fdd015be03c084390358cb565b5721a4868e423c
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-04-11 02:44:47 +0530

    Add files via upload

--- Day46_TimeNeededtoBuyTickets.java
+++ Day46_TimeNeededtoBuyTickets.java
@@ -3,8 +3,41 @@ package LeetCode75;
 public class Day46_TimeNeededtoBuyTickets {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[]tickets= {5,1,1,1};
+		int k=0;
+		System.out.println(timeRequiredToBuy(tickets, k));
+		System.out.println(alter(tickets, k));
+	}
+	
+	
+	static int alter(int[]tickets,int k)
+	{
+		int time=0;
+		for(int i=0; tickets[k]>0;i++)
+		{
+			if(tickets[i]>0)
+				{
+				tickets[i]--;
+				time++;
+				}
+			
+			if(i==tickets.length-1)i=-1;
+		}
+		return time;
 	}
 
+	
+	 public static int timeRequiredToBuy(int[] tickets, int k) {
+	        int total = 0;
+
+	        for (int i = 0; i < tickets.length; i++) {
+	            if (i <= k) {
+	                total += Math.min(tickets[i], tickets[k]);
+	            } else {
+	                total += Math.min(tickets[i], tickets[k] - 1);
+	            }
+	        }
+
+	        return total;
+	    }
 }
