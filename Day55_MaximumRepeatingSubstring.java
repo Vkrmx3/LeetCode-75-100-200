3193aa1753442e3f126b3cbb42b2f0a79a5485af
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-04-18 23:09:39 +0530

    Add files via upload

--- Day55_MaximumRepeatingSubstring.java
+++ Day55_MaximumRepeatingSubstring.java
@@ -3,8 +3,7 @@ package LeetCode75;
 public class Day55_MaximumRepeatingSubstring {
 
 	public static void main(String[] args) {
-		String sequence = "bacaaaacabcaccccaacbcccccbcaaaacaabbaabbcababacca",
-				word = "ac";
+		String sequence = "bacaaaacabcaccccaacbcccccbcaaaacaabbaabbcababacca", word = "ac";
 		System.out.println(maxRepeating(sequence, word));
 	}
 
@@ -13,16 +12,15 @@ public class Day55_MaximumRepeatingSubstring {
 		for (int i = 0; i < sequence.length(); i++) {
 			count = 0;
 
-			for (int j = i; j < sequence.length() - (word.length()-1); j++) {
+			for (int j = i; j < sequence.length() - (word.length() - 1); j++) {
 				if (sequence.charAt(j) != word.charAt(0)) {
 					count = 0;
-				}
-				else if (word.equals(sequence.substring(j, j + word.length()))) {
+				} else if (word.equals(sequence.substring(j, j + word.length()))) {
 					count++;
 					j += word.length() - 1;
 					max = Math.max(max, count);
-				}
-				else count=0;
+				} else
+					count = 0;
 			}
 		}
 		return max;
