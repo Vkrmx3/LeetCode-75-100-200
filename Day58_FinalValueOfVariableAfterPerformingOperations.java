7af6cb2afa89dd94fa7e630016be026eda2dcade
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-04-21 19:32:29 +0530

    Add files via upload

---
+++ Day58_FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,20 @@
+package LeetCode75;
+
+public class Day58_FinalValueOfVariableAfterPerformingOperations {
+
+	public static void main(String[] args) {
+		String[]operations= {"--X","X++","X++"};
+		System.out.println(finalValueAfterOperations(operations));
+	}
+	public static int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String s: operations)
+        {
+            if(s.charAt(1)=='+')
+                x++;
+           else
+                --x;
+        }
+        return x;
+    }
+}
