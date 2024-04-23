fe2cbf36e2123dd8c793c44657757b69d49f5f1d
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-04-23 23:31:23 +0530

    Add files via upload

---
+++ Day60_BuildArrayFromPermutation.java
@@ -0,0 +1,19 @@
+package LeetCode75;
+
+import java.util.Arrays;
+
+public class Day60_BuildArrayFromPermutation {
+
+	public static void main(String[] args) {
+		int[]nums= {0,2,1,5,3,4};
+		System.out.println(Arrays.toString(buildArray(nums)));
+	}
+	public static int[] buildArray(int[] nums) {
+        int[]ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
+    }
+}
