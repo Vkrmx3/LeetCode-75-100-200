85d3ce40b717d226b81d48c982aff0997d1125e4
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-04-25 23:33:27 +0530

    Add files via upload

---
+++ Day62_LargestPositiveIntegerThatExistsWithItsNegative.java
@@ -0,0 +1,24 @@
+package LeetCode75;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day62_LargestPositiveIntegerThatExistsWithItsNegative {
+
+	public static void main(String[] args) {
+		int[]nums= {-9,-43,24,-23,-16,-30,-38,-30};
+		System.out.println(findMaxK(nums));
+	}
+	public static int findMaxK(int[] nums) {
+        int result=-1;
+       for(int i=0;i<nums.length;i++)
+       {
+        for(int j=i+1;j<nums.length;j++)
+        {
+            if(nums[i]+nums[j]==0)
+            result=Math.max(result, Math.abs(nums[i]));
+        }
+       }
+        return result;
+    }
+}
