9465c39111259c010641b3191429f29280f6117a
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-05-15 23:51:55 +0530

    Add files via upload

---
+++ Day82_2710_RemoveTrailingZerosFromaString.java
@@ -0,0 +1,17 @@
+package LeetCode75;
+
+public class Day82_2710_RemoveTrailingZerosFromaString {
+
+	public static void main(String[] args) {
+		System.out.println(removeTrailingZeros("51230100"));
+	}
+	public static String removeTrailingZeros(String num) {
+		int i=num.length()-1;
+		for(;i>=0;i--)
+        {
+        	if(num.charAt(i)!='0')
+        		break;
+        }
+        return num.substring(0,i+1);
+    }
+}
