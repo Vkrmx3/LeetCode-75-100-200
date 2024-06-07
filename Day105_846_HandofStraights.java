b5b7519b749bbf278a9ec9e66457c901c792addc
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-07 23:49:26 +0530

    Update Day105_846_HandofStraights.java

--- Day105_846_HandofStraights.java
+++ Day105_846_HandofStraights.java
@@ -4,7 +4,7 @@ public class Day846_HandofStraights {
 
 	public static void main(String[] args) {
 		int[] hand = {1,2,3,6,2,3,4,7,8 };
-    int groupSize=3;
+   		int groupSize=3;
 		System.out.println(isNStraightHand(hand,groupSize));
 	}
 public static boolean isNStraightHand(int[] hand, int groupSize) {
