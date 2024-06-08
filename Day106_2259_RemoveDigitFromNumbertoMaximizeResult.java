c378725a0f37c35c74360c222d0b329bc9f10988
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-08 17:56:17 +0530

    Create Day106_2259_RemoveDigitFromNumbertoMaximizeResult.java

---
+++ Day106_2259_RemoveDigitFromNumbertoMaximizeResult.java
@@ -0,0 +1,22 @@
+public class Day106_2259_RemoveDigitFromNumbertoMaximizeResult
+{
+    public static void main(String[]args)
+    {
+      String number="1231", digit="1";
+        System.out.println(removeDigit(number,digit));
+    } 
+    public static String removeDigit(String number, char digit) {
+        String result="";
+        for(int i=0;i<number.length();i++)
+        {
+            if(number.charAt(i)==digit)
+            {
+                String temp=number.substring(0,i)+number.substring(i+1,number.length());
+                if(temp.compareTo(result)>0)
+                    result=temp;
+                
+            }
+        }
+        return result;
+    }
+}
