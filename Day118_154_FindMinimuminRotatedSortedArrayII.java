cb2578cce10e5534ba2edc3c1c5ddf734e844a58
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-20 22:58:06 +0530

    Create Day118_154_FindMinimuminRotatedSortedArrayII.java

---
+++ Day118_154_FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,29 @@
+public class Day118_154_FindMinimuminRotatedSortedArrayII
+  {
+    public static void main(String[]args)
+    {
+      int[]nums={12,23,34,45,56,67,45,1,22,22,33,44,55,55,56,56,76,1,2,3,4,5,6,7,8,9,11};
+      System.out.println(findMin(nums));
+    }
+    public static int findMin(int[] nums) {
+        int start=0, end=nums.length-1, min=Integer.MAX_VALUE;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            min=Math.min(min, nums[mid]);
+            if(nums[start]==nums[mid] && nums[mid]==nums[end])
+            {
+                start++;
+                end--;
+            }
+            else if(nums[start]<=nums[mid])
+            {
+            	min=Math.min(min, nums[start]);
+            	start=mid+1;
+            }
+            else
+            	end=mid-1;        
+        }
+        return min;
+    }
+  }
