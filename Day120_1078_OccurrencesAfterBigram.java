2210ccbf9501993f3b5ef26d75e8113b40e5ac80
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-22 23:25:20 +0530

    Update Day120_1078_OccurrencesAfterBigram.java

--- Day120_1078_OccurrencesAfterBigram.java
+++ Day120_1078_OccurrencesAfterBigram.java
@@ -3,7 +3,7 @@ class Solution {
         List<String>list=new ArrayList();
         String[]arr=text.split(" ");
         for(int i=0; i<arr.length-2; i++)
-        {
+        { 
             if(arr[i].equals(first) && arr[i+1].equals(second))
                 list.add(arr[i+2]);
         }
