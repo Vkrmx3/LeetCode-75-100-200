63cfc7e259b3bbd8d2b3ce3ddf3b223c644c9a7f
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-23 18:41:52 +0530

    Create Day121_2089_FindTargetIndicesAfterSortingArray.java

---
+++ Day121_2089_FindTargetIndicesAfterSortingArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+
+        int count = 0, lessthan = 0;
+        for (int ele : nums) {
+            if (ele == target)
+                count++;
+            else if (ele < target)
+                lessthan++;
+        }
+        List<Integer> list = new ArrayList();
+        while (count > 0) {
+            list.add(lessthan++);
+            count--;
+        }
+        return list;
+    }
+}
