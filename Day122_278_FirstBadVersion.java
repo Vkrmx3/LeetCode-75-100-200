3a54a1b5c57f78c3705070d0bbf5d02f2fa7c442
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-24 22:54:38 +0530

    Update Day122_278_FirstBadVersion.java

--- Day122_278_FirstBadVersion.java
+++ Day122_278_FirstBadVersion.java
@@ -4,7 +4,6 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start=n%2==0?0:1, end=n, result=Integer.MAX_VALUE;
-        
         while(start<end)
         {
             n=start+(end-start)/2;
