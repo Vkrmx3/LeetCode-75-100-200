36ee67e7cf4625f8a4874cd5191be223be0cde74
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-28 23:53:05 +0530

    Update Day126_540_SingleElementinaSortedArray.java

--- Day126_540_SingleElementinaSortedArray.java
+++ Day126_540_SingleElementinaSortedArray.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int start=0, end=nums.length-1;
         while(start<end)
-        { 
+        {
             int mid = start+(end-start)/2;
             if(mid%2!=0)
                 mid--;
