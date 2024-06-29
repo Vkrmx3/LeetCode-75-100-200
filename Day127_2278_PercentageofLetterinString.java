e8c9ed6fab6ad36a635cff261078b9f5dc5d7634
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-29 23:26:00 +0530

    Update Day127_2278_PercentageofLetterinString.java

--- Day127_2278_PercentageofLetterinString.java
+++ Day127_2278_PercentageofLetterinString.java
@@ -1,7 +1,7 @@
 class Solution {
     public int percentageLetter(String s, char letter) {
          double count=0;
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<s.length(); i++) 
             if(s.charAt(i)==letter)
                 count++;
         return (int)((count/s.length())*100);
