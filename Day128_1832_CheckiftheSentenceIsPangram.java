4641b7a4aad44b5c367486fe0250c9d9b12a126c
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-06-30 22:10:16 +0530

    Update Day128_1832_CheckiftheSentenceIsPangram.java

--- Day128_1832_CheckiftheSentenceIsPangram.java
+++ Day128_1832_CheckiftheSentenceIsPangram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean checkIfPangram(String sentence) {
         if(sentence.length()<26)
             return false;
-        Set<Character>set=new HashSet();
+        Set<Character>set=new HashSet(); 
         for(char c: sentence.toCharArray())
             set.add(c);
         return set.size()==26;
