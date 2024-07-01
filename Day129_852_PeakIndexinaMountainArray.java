6babe9256743e830857d77347b811d7070f53098
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-07-01 23:10:39 +0530

    Create Day129_852_PeakIndexinaMountainArray.java

---
+++ Day129_852_PeakIndexinaMountainArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start=0, end=arr.length-1, mid=start+(end-start)/2;;
+        while(start<end)
+        {
+            mid=start+(end-start)/2;
+            if(arr[mid]<arr[mid-1])
+                end=mid;
+            else
+             start=mid+1;
+        }
+        return start-1;
+    }
+}
