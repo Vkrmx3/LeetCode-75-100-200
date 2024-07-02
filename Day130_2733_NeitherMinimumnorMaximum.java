0bafca901b88e0e363bf5951633c1484fda09454
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-07-02 22:45:44 +0530

    Create Day130_2733_NeitherMinimumnorMaximum.java

---
+++ Day130_2733_NeitherMinimumnorMaximum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if(nums.length<3)
+        return -1;
+        int min=Integer.MAX_VALUE, result=min;
+        for(int ele: nums)
+        {
+            if(ele<min)
+            {
+                result=min;
+                min=ele;
+            }
+            else if(ele<result)
+            {
+                result=ele;
+            }
+        }
+        return result;
+    }
+}
