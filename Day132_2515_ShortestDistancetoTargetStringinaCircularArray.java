052b78915cd662dab0a589428f3464d776b3a2a6
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-07-04 23:27:51 +0530

    Update Day132_2515_ShortestDistancetoTargetStringinaCircularArray.java

--- Day132_2515_ShortestDistancetoTargetStringinaCircularArray.java
+++ Day132_2515_ShortestDistancetoTargetStringinaCircularArray.java
@@ -5,7 +5,7 @@ class Solution {
        {
         if(words[left].equals(target))
             return leftcount;
-        if(words[right].equals(target))
+        if(words[right].equals(target)) 
             return rightcount; 
             left=((left-1)+n)%n;
             right=(right+1)%n;
