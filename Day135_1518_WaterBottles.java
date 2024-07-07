a310ce9a018f1d82788caac370db173477828b6a
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-07-07 22:30:19 +0530

    Create Day135_1518_WaterBottles.java

---
+++ Day135_1518_WaterBottles.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int empty=numBottles;
+        while(empty>=numExchange )
+        {
+            empty-=numExchange-1;
+            numBottles++;
+        }
+        returnS numBottles;
+    }
+}
