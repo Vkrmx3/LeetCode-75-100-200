010218394f22652629e395b89576777ef95001c7
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-07-08 23:27:53 +0530

    Create Day136_1823_FindtheWinneroftheCircularGame.java

---
+++ Day136_1823_FindtheWinneroftheCircularGame.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        List<Integer>list=new ArrayList();
+        for(int i=1;i<=n;i++)
+        list.add(i);
+        int current=0;
+        while(list.size()>1)
+        {
+            int next= (current+k-1)%list.size();
+            list.remove(next);
+            current=next;
+        }
+        return list.get(0);
+    }
+}
