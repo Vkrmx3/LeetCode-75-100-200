3a512440476806db0db630d49e2f5ef60cfb6909
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-07-09 22:42:29 +0530

    Create Day137_1701_AverageWaitingTime.java

---
+++ Day137_1701_AverageWaitingTime.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double averageWaitingTime(int[][] customers) {
+        int chef=1;
+        double avg=0;
+        for(int i=0; i<customers.length; i++)
+        {
+            chef=Math.max(chef, customers[i][0])+ customers[i][1];
+            avg+= chef-customers[i][0];
+        }
+    return avg/customers.length;
+    }
+}
