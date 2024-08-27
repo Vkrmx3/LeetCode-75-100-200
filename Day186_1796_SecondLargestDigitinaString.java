9085227e93499332e76f176b7fc24f787eb99592
VISHNUKUMAR T <87655534+Vkrmx3@users.noreply.github.com>
2024-08-27 23:42:46 +0530

    Create Day186_1796_SecondLargestDigitinaString.java

---
+++ Day186_1796_SecondLargestDigitinaString.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int secondHighest(String s) {
+        Set<Integer>set=new TreeSet();
+        for(int i=0;i<s.length();i++)
+            if(s.charAt(i)>='0'&& s.charAt(i)<='9')
+                set.add(Integer.parseInt(s.substring(i,i+1)));
+        if(set.size()>1)
+        {
+            int count=0;
+            for(int ele:set)
+            {
+                count++;
+                if(count==set.size()-1)
+                    return ele;
+            }
+        }
+        return -1;
+    }
+}
